21feaf23919f2f199d9ac373f02f11c4509275ad
Shelby Gluck <shelbygluck@gmail.com>
2020-04-25 15:30:26 -0400

    toggle buttons balanced

--- app.js
+++ app.js
@@ -1,12 +1,73 @@
-let chosenCompany = null
+let chosenCompany = 'none'
 
-let buttons2 = document.getElementsByTagName('input')
-for (let i = 0; i < buttons2.length; i++) {
-    buttons2[i].addEventListener("click", switchButton)
+let microsoftToggle = false
+let microsoftButton = document.getElementById('microsoft')
+microsoftButton.addEventListener("click", switchMicrosoft)
+
+function switchMicrosoft () {
+    if (microsoftToggle) {
+      microsoftToggle = false;
+      if (!appleToggle && !googleToggle && !amazonToggle) {chosenCompany = 'none'}
+    } else {
+        microsoftToggle = true
+        chosenCompany = 'microsoft'
+        if (amazonToggle) {amazonButton.click()}
+        if (googleToggle) {googleButton.click()}
+        if (appleToggle) {appleButton.click()}
+    }
+    console.log(chosenCompany)
 }
 
-function switchButton () {
-    console.log('here')
+let amazonToggle = false
+let amazonButton = document.getElementById('amazon')
+amazonButton.addEventListener("click", switchAmazon)
+
+function switchAmazon () {
+    if (amazonToggle) {
+      amazonToggle = false;
+      if (!microsoftToggle && !googleToggle && !appleToggle) {chosenCompany = 'none'}
+    } else {
+        amazonToggle = true
+        chosenCompany = 'amazon'
+        if (microsoftToggle) {microsoftButton.click()}
+        if (googleToggle) {googleButton.click()}
+        if (appleToggle) {appleButton.click()}
+    }
+    console.log(chosenCompany)
 }
 
-//onchange, setattribute value for true ? false, others for false, chosencompany for company
+let googleToggle = false
+let googleButton = document.getElementById('google')
+googleButton.addEventListener("click", switchGoogle)
+
+function switchGoogle () {
+    if (googleToggle) {
+      googleToggle = false;
+      if (!microsoftToggle && !appleToggle && !amazonToggle) {chosenCompany = 'none'}
+    } else {
+        googleToggle = true
+        chosenCompany = 'google'
+        if (amazonToggle) {amazonButton.click()}
+        if (microsoftToggle) {microsoftButton.click()}
+        if (appleToggle) {appleButton.click()}
+    }
+    console.log(chosenCompany)
+}
+
+let appleToggle = false
+let appleButton = document.getElementById('apple')
+appleButton.addEventListener("click", switchApple)
+
+function switchApple () {
+    if (appleToggle) {
+      appleToggle = false;
+      if (!microsoftToggle && !googleToggle && !amazonToggle) {chosenCompany = 'none'}
+    } else {
+        appleToggle = true
+        chosenCompany = 'apple'
+        if (amazonToggle) {amazonButton.click()}
+        if (googleToggle) {googleButton.click()}
+        if (microsoftToggle) {microsoftButton.click()}
+    }
+    console.log(chosenCompany)
+}
